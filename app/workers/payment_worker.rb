e727b08124607daa32f9e97d75b9150b2cb74241
Shanon McQuay <shanonmcquay@gmail.com>
2016-09-15 12:16:42 +1000

    Add a way to benchmark different steps of the payment processing.

--- app/workers/payment_worker.rb
+++ app/workers/payment_worker.rb
@@ -2,13 +2,39 @@ class PaymentWorker
   include Sidekiq::Worker
 
   def perform(payload)
-    data = payload.with_indifferent_access.fetch(:data)
+    Steps.new(payload).execute
+  end
 
-    account_id      = data.fetch(:account_id)
-    current_balance = data.fetch(:transaction).fetch(:current_balance)
+  class Steps
+    def initialize(payload)
+      @payload = payload
+    end
 
-    Device.includes(:accounts).where(accounts: { external_id: account_id,}, wants_notifications: true).each do |device|
-      DeviceNotifier.notify_balance_change(device, current_balance)
+    def execute
+      extract
+      Benchmark.bm do |bm|
+        bm.report('filter') { filter }
+      end
+      notify
+    end
+
+    private
+
+    def extract
+      data = @payload.with_indifferent_access.fetch(:data)
+
+      @account_id      = data.fetch(:account_id)
+      @current_balance = data.fetch(:transaction).fetch(:current_balance)
+    end
+
+    def filter
+      @filtered = Device.includes(:accounts).where(accounts: { external_id: @account_id,}, wants_notifications: true).to_a
+    end
+
+    def notify
+      @filtered.each do |device|
+        DeviceNotifier.notify_balance_change(device, @current_balance)
+      end
     end
   end
 end
